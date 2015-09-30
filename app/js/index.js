34713778dcdfe6f3f83929545f640a84b8f5907a
kahjapan <kaho.iguchi@gmail.com>
2015-09-30 13:00:11 +0900

    informationとfooter作った

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,33 @@
-function hoge(){
-  console.log();
-}
+$(function() {
+  var a = $('#top');
+  var b = $('#information');
+  var yokoneVR = $('.yokoneVR');
+  var itext = $('.itext');
+  //スクロールしたら文字が消えてVRが出るやつ
+  yokoneVR.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > a.height() ) {
+      // setTimeout(function(){
+        console.log("A");
+      itext.fadeOut();
+      yokoneVR.fadeIn();
+    // },3000);
+    return false;
+    } else {
+      console.log("B");
+        itext.fadeIn();
+        yokoneVR.fadeOut();
+    }
+  });
+  //クリックしたらスクロールするやつ
+    $('a[href^=#]').click(function(){
+        var speed = 500; //移動完了までの時間(sec)を指定
+        var href= $(this).attr("href");
+        var target = $(href === "#" || href === "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+
+
+});
